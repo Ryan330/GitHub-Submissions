8c888acb2f1ae72844d8474b7fa7d7aa9d90b025
“[Ryan <[ryanmiller12@bellsouth.net]>
2018-04-30 11:36:51 -0400

    SortList

---
+++ sortlistfunction.py
@@ -0,0 +1,13 @@
+#Sort List Function
+numlist = [20, 50, 30, 10, 60, 90, 80, 40, 100, 70,]
+strlist = ["third", "first", "second"]
+
+def sortlist(s):
+    s.sort()
+    return 
+
+sortlist(numlist)
+sortlist(strlist)
+
+print(numlist)
+print(strlist)
